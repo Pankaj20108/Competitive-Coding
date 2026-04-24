0f40b419f56c2432a6460a6c49bf8751cd86b371
iampankaj <pankajsuthar20108@gmail.com>
2026-04-24 10:57:44 +0530

    Update 1590.cpp

--- Lab_MST/1590.cpp
+++ Lab_MST/1590.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        if(k==0||k==1) return 0;
-        int count=0;
-        long long prod=1;
-        int first=0;
-        int second=0;
-        while(second<nums.size()){
-            prod*=nums[second];
-            while(prod>=k){
-                prod/=nums[first];
-                first++;
-            }
-            count+=(second-first+1);
-            second++;
+    int minSubarray(vector<int>& nums, int p) {
+        long long sum=accumulate(nums.begin(), nums.end(), 0LL);
+        int n=nums.size();
+        if(sum%p==0){
+            return 0;
         }
-        return count;
+        unordered_map<int,int>m;
+        int rem=sum%p;
+        int minlen=n;
+        m[0]=-1;
+        long long prefix=0;
+        for(int i=0;i<n;i++){
+            prefix+=nums[i];
+            int val=prefix%p;
+            int mod=(val-rem+p)%p;
+            if(m.find(mod)!=m.end()){
+                minlen=min(minlen, i-m[mod]);
+            }
+            m[val]=i;  
+        }
+        if(minlen==n) return -1;
+        return minlen;
         
     }
 };
